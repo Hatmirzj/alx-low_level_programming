e158c4d59ac3f0e272627717877e45b294c19b44
Hatmirzj <hatmirzj@gmail.com>
2022-06-08 06:37:09 -0700

    putchar file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H_INCLUDE
+#define MAIN_H_INCLUDE
+
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -6,8 +9,10 @@ int _isalpha(int);
 int print_sign(int);
 int _abs(int);
 int print_last_digit(int);
-void am_one(void);
+void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int);
 void print_times_table(int);
+
+#endif
