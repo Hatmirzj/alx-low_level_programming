4228e9baba558a2af74171f62a4dee3086f89124
Hatmirzj <hatmirzj@gmail.com>
2022-06-09 23:56:51 -0700

    deb

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -1,14 +1,16 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
 int main(void)
-
 {
+	int i;
 
-	positive_or_negative( );
+	i = 98;
+	positive_or_negative(i);
+
 	return (0);
 }
