fc9f14f1f837e892747f06512ccd10c3e7c6f307
Hatmirzj <hatmirzj@gmail.com>
2022-06-14 23:15:39 -0700

    0x05-pointer_array

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H_INCLUDE
 #define MAIN_H_INCLUDE
 
+int _atoi(char *s);
+char *_strcpy(char *dest, char *src);
+void print_array(int *a, int n);
+void puts_half(char *str);
+void rev_string(char *s);
+void puts2(char *str);
 int _putchar(char);
 void print_rev(char *s);
 void print_alphabet(void);
